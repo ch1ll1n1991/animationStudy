696f6c597f9345ea9d3a4764ecd2719363ae3b98
ch1ll1n1991 <shinya.tsuchikura@gmail.com>
2024-12-18 23:08:54 +0900

    class switch

--- script.js
+++ script.js
@@ -3,4 +3,10 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
   const menu = document.querySelector('.menu');
   menu.classList.toggle('active');
+  const hamburger = document.querySelector('.hambuger');
+  if (!hamburger.classList.contains("is-active")){
+    hamburger.classList.add("is-active");
+  } else {
+    hamburger.classList.remove("is-active");
+  }
 });
