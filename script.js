c044f264a3c5da1506148e4d58e3da54854184fa
ch1ll1n1991 <shinya.tsuchikura@gmail.com>
2024-12-18 23:20:26 +0900

    add forEach

--- script.js
+++ script.js
@@ -4,9 +4,5 @@ document.querySelector('.hamburger').addEventListener('click', () => {
   const menu = document.querySelector('.menu');
   menu.classList.toggle('active');
   const hamburger = document.querySelector('.hambuger');
-  if (hamburger.classList.contains("is-active")){
-    hamburger.classList.remove("is-active");
-  } else {
-    hamburger.classList.add("is-active");
-  }
+  hamburger.forEach((item) => item.classList.toggle("is-active"));
 });
